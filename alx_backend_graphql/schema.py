4d14b45f6854e7ba5c1d4274968d949c824fc9e1
moment49 <ibenachoelvis49@gmail.com>
2025-08-30 02:20:34 +0100

    update: worked on the CreateOrder Muatation class to create nested order for a products and customer

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -3,8 +3,8 @@ from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 
 class Query(CRMQuery, graphene.ObjectType):
-    # hello = graphene.String(default_value="Hello, GraphQL!")
-    pass
+    hello = graphene.String(default_value="Hello, GraphQL!")
+    
 
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
---
+++ crm/filters.py
@@ -0,0 +1,45 @@
+from .models import Product, Customer, Order
+import django_filters
+
+
+class CustomerFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    email = django_filters.CharFilter(field_name='email', lookup_expr='icontains')
+    created_at__gte = django_filters.DateFilter(field_name='created_at', lookup_expr='gte')
+    created_at__lte = django_filters.DateFilter(field_name='created_at', lookup_expr='lte')
+    phone_pattern = django_filters.CharFilter(method='phone_pattern')
+   
+
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'phone', 'created_at']
+    
+    def phone_pattern(self, queryset, name, value):
+        return queryset.filter(phone__startswith=value)
+    
+class ProductFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    price = django_filters.NumberFilter(field_name='price', lookup_expr='incontains')
+    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
+    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    stock = django_filters.NumberFilter(field_name='stock', lookup_expr='icontains')
+    stock__gt = django_filters.NumberFilter(field_name='stock', lookup_expr='gt')
+    stock__lt = django_filters.NumberFilter(field_name='stock', lookup_expr='lt')
+    
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'stock']
+
+class OrderFilter(django_filters.FilterSet):
+    customer__name = django_filters.CharFilter(field_name='customer__name', lookup_expr='customer__name')
+    total_amount__gte = django_filters.NumberFilter(field_name='total_amount', lookup_expr='gt')
+    total_amount__lte = django_filters.NumberFilter(field_name='total_amount', lookup_expr='lt')
+    total_amount = django_filters.NumberFilter(field_name='total_amount', lookup_expr='icontains')
+    product_name = django_filters.CharFilter(field_name='product__name', lookup_expr='product__name')
+    order_date = django_filters.DateTimeFilter(field_name='order_date', lookup_expr='icontains')
+    
+    class Meta:
+        model = Order
+        fields = ['customer', "products", "order_date", "total_amount"]
+
+    
